3b6f57f399357575e792013ba9bd4931d9e879c0
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-06-14 23:32:03 +0400

    task 1 solve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,11 +17,11 @@ public class Main {
         int n = sc.nextInt();
 
         System.out.println(
-                n<1 || n>12 ? "Yanlış ay":
-                        n==12 || n==1 || n==2 ? "Qis" :
-                                n>=3 && n<=5 ? "Yaz" :
-                                        n>=6 && n<=8 ? "Yay" :
-                                                "Payiz"
+                n<1 || n>12 ? "Yanlış ay" :
+                n==12 || n==1 || n==2 ? "Qis" :
+                n<=5 ? "Yaz" :
+                n<=8 ? "Yay" :
+                "Payiz"
         );
     }
 }
