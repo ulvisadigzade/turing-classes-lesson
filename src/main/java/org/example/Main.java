5a6ec07e469412b7ae7b62e225c418f1306c1d69
Ulvi Sadigzade <ulvisadiqzade05@gmail.com>
2025-06-19 10:15:41 +0400

    add third task to lesson04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
 
+import org.example.lesson04.MultiplicationTableGenerator;
 import org.example.lesson04.StudentGradeEvaluationSystem;
 import org.example.lesson04.VendingMachineProductSelector;
 
 public class Main {
     public static void main(String[] args) {
-        StudentGradeEvaluationSystem.evaluate();
+        //StudentGradeEvaluationSystem.evaluate();
         //VendingMachineProductSelector.selector();
+        //MultiplicationTableGenerator.generator();
     }
 }
---
+++ src/main/java/org/example/lesson04/MultiplicationTableGenerator.java
@@ -0,0 +1,16 @@
+package org.example.lesson04;
+
+import java.util.Scanner;
+
+public class MultiplicationTableGenerator {
+    public static void generator(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number N:");
+        int N = sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+    }
+}
